87b3c118dff331daadf686ae2e10b9e6da762cd9
Davinia pd <delgadavi@gmail.com>
2024-08-05 19:10:24 +0000

    creadas las casillas de cada numero y el icono del temporizador. Todo aun estático

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,6 +4,7 @@ import SecondsCounter from "./secondsCounter";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
 	return (
--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -1,9 +1,21 @@
+
 import React from "react";
+
 
 const SecondsCounter = () => {
     return (
-        <h1>secondCOUNTER</h1>
-    )
+        <>
+            <div className="d-flex justify-content-evenly m-5 p-5 bg-warning" style={{fontSize: "200px"}}>
+                <div><i class="fa-solid fa-stopwatch"></i></div>
+                <div>6</div>
+                <div>5</div>
+                <div>4</div>
+                <div>3</div>
+                <div>2</div>
+                <div>1</div>
+            </div>
+        </>
+    );
 };
 
 export default SecondsCounter;
