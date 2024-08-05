2e5b50e84c3cf28453a4eb7c4e030317d4bc8904
Davinia pd <delgadavi@gmail.com>
2024-08-05 19:24:36 +0000

    definidos los props y proprTypes de secondsCounter

--- src/js/component/secondsCounter.jsx
+++ src/js/component/secondsCounter.jsx
@@ -1,21 +1,33 @@
-
 import React from "react";
+import PropTypes from 'prop-types';
 
 
-const SecondsCounter = () => {
+const SecondsCounter = (props) => {
     return (
         <>
             <div className="d-flex justify-content-evenly m-5 p-5 bg-warning" style={{fontSize: "200px"}}>
-                <div><i class="fa-solid fa-stopwatch"></i></div>
-                <div>6</div>
-                <div>5</div>
-                <div>4</div>
-                <div>3</div>
-                <div>2</div>
-                <div>1</div>
+                <div><i className="fa-solid fa-stopwatch"></i></div>
+                <div>{props.digitSix}</div>
+                <div>{props.digitFive}</div>
+                <div>{props.digitFour}</div>
+                <div>{props.digitThree}</div>
+                <div>{props.digitTwo}</div>
+                <div>{props.digitOne}</div>
             </div>
         </>
     );
 };
+
+SecondsCounter.prototypes = {
+    digitOne: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitFour: PropTypes.number,
+    digitFive: PropTypes.number,
+    digitSix: PropTypes.number
+};
+
+
+
 
 export default SecondsCounter;
